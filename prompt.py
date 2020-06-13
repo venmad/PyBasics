929344e39b6dffde50cc4747b660a2cdc0614887
venmad <venumadhav888@gmail.com>
2020-06-13 19:25:02 +0530

    Fist commit

---
+++ prompt.py
@@ -0,0 +1,29 @@
+# program to explain the usage of prompt inputs
+def a():
+    x = int(input('Enter the First Number'))
+    y = int(input('Enter the second Number'))
+    z = x + y
+    print('The sum of tow number is ', z)
+
+
+# Fetch the character index
+def b():
+    ch = input('Enter a character')
+    print(ch[0])
+
+
+# Pass inputs via arguments
+
+import sys
+
+
+def c():
+    x = sys.argv[1]
+    y = sys.argv[2]
+    z = x + y
+    print(z)
+
+
+a()
+b()
+#c()
